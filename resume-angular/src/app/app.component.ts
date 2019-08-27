1e30594b08b7fc06d04ac2563c91087c146e5e8a
Mason Richardson <mason.p.richardson@gmail.com>
2019-08-27 01:40:01 -0700

    no message

--- resume-angular/src/app/app.component.ts
+++ resume-angular/src/app/app.component.ts
@@ -1,48 +1,106 @@
-import { Component, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import {TweenMax, Expo, Power2} from "gsap/TweenMax";
 
 export interface DialogData {
     animal: string;
     name: string;
-  }
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test';
+
+    constructor(private router: Router) { }
+
+    title = 'Mason Richardson';
+    animal: string;
+    name: string;
+
+    skills: any[] = [
+        "UI/UX Design",
+        "MySQL",
+        "Digital Ocean",
+        "AWS",
+        "Linux",
+        "Git Version Control",
+        "Angular",
+        "NodeJS",
+        "REST API"
+    ]
+
+    mailText: string = "";
 
 
+    ngOnInit() {
+        this.mailText = "mailto:mason.p.richardson@gmail.com?subject=I saw your website!";
 
-  animal: string;
-  name: string;
-
-  skills: any[] = [
-    "UI/UX Design",
-    "MySQL",
-    "Digital Ocean",
-    "AWS",
-    "Linux",
-    "Git Version Control",
-    "Angular",
-    "NodeJS",
-    "REST API"
-  ]
-
-  mailText:string = "";
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0)
+        });
+    }
 
 
-  ngOnInit(){
-    this.mailText = "mailto:mason.p.richardson@gmail.com?subject=I saw your website!";
-  }
+    SendEmail(): void {
+        this.mailText = "mailto:mason.p.richardson@gmail.com?subject=I saw your website!";
+        window.location.href = this.mailText;
+    }
+
+    fadeOut(): void {
+        TweenMax.to(".myBtn", 1, {
+            y: -100,
+            opacity: 0
+        });
+
+        TweenMax.to(".screen", 2, {
+            y: -400,
+            opacity: 0,
+            ease: Power2.easeInOut,
+            delay: 2
+        });
+
+        TweenMax.from(".overlay", 2, {
+            ease: Power2.easeInOut
+        });
+
+        TweenMax.to(".overlay", 2, {
+            delay: 2.6,
+            top: "-110%",
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.to(".overlay-2", 2, {
+            delay: 3,
+            top: "-110%",
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from(".content", 2, {
+            delay: 3.2,
+            opacity: 0,
+            ease: Power2.easeInOut
+        });
 
 
-  SendEmail(): void {
-    this.mailText = "mailto:mason.p.richardson@gmail.com?subject=I saw your website!";
-    window.location.href = this.mailText;
-  }
+        TweenMax.to(".content", 2, {
+            opacity: 1,
+            y: 0,
+            delay: 3.2,
+            ease: Power2.easeInOut
+        });
 
-  
+        setTimeout(function () {
+            var element = document.getElementById("body");
+            element.classList.remove("no-scroll");
+        }, 3500);
+    }
+
+
 }
 
